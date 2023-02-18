8199d42595a64b0c3324b46c9fc388081399064d
Steve Yonkeu <yokwejuste@yahoo.com>
2023-02-18 14:01:42 +0100

    feat: user model (#6)
    * feat: added user model
    
    * feat: added user model

---
+++ lib/models/users.dart
@@ -0,0 +1,34 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:flutter/material.dart";
+
+class User {
+  late String userId;
+  late String username;
+  late String email;
+  late String phone;
+  late String name;
+  late String department;
+  late String type;
+  late int level;
+  late String gender;
+  late String school;
+  late Timestamp createdAt;
+  late Timestamp dateOfBirth;
+  late String whatsappGroup;
+
+  User({
+    required this.userId,
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.name,
+    required this.department,
+    required this.type,
+    required this.level,
+    required this.gender,
+    required this.school,
+    required this.createdAt,
+    required this.dateOfBirth,
+    required this.whatsappGroup,
+  });
+}
