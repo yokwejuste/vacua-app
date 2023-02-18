4b7bf427b8492e07a3156e214f5a909f5e2f1d70
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-02-18 10:57:55 +0100

    feat: project structure created

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text("Login Screen")
+        ),
+      ),
+    );
+  }
+}
