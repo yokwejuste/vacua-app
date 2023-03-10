77ba5ea4b25a59bde160e98d21b0a08dcb448e13
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-10 05:43:40 +0100

    localization packages added

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,3 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-const Color kPrimaryColor = Color(0xFF845EC2);
-=======
 const Color kPrimaryColor = Color.fromRGBO(146, 29, 218, 1);
->>>>>>> c94bd9c53002b7fc49c0a8cbd88f51d1bb51cf92
