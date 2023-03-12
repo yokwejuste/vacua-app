8b18ed5cc5d5d26454c0f79fa33d76c7fb856283
Steve Yonkeu <yokwejuste@gmail.com>
2023-03-12 07:07:34 +0100

    feat: seeting page and context error fixed

---
+++ lib/screens/searchHall.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class SearchHall extends StatelessWidget {
+  const SearchHall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Icon(
+          Icons.search,
+          size: 100,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
