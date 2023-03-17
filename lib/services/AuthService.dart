fcab460aac14ff11a4de3d35189024a4f3372ca9
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-03-17 06:04:54 +0100

    feat: user login function added

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -2,28 +2,30 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 // login services
 
-class AuthServices{
+class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   // Login user
   // will only return a string if there is an error
   Future login(String email, String password) async {
     try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       User? user = result.user;
       // Todo - get user exra details and redirect to their respective screens
     } on FirebaseAuthException catch (e) {
-      if(e.code == 'user-not-found' || e.code == 'wrong-password'){
-        return 'Invalid email or password';
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        throw 'Invalid email or password';
       } else {
-        return 'There was a problem logging in. Pleas try again later';
+        throw 'There was a problem logging in. Pleas try again later';
       }
     }
   }
 
   Future Register(String email, String password) async {
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       User? user = result.user;
       return user;
     } catch (e) {
@@ -41,4 +43,4 @@ class AuthServices{
       return null;
     }
   }
-} 
+}
